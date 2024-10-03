0fb9f8468877662d8199f3f4528f50f06fd16841
Spencer Lepine <60903378+spencerlepine@users.noreply.github.com>
2024-10-03 20:15:49 +0300

    skeleton Footer w/o styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,38 @@
+import config from "@/config";
+import Image from "next/image";
+
 export default function Footer() {
   return (
     <footer>
-      <h1>🏗️ Work in progress</h1>
+      <div>
+        <ul>
+          {config.footerLinks.map((link) => (
+            <li key={link.name}>
+              <a href={link.url} target="_blank" rel="noopener noreferrer">
+                {link.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        <ul>
+          {config.socialMedias.map((icon) => (
+            <li key={icon.name}>
+              <a href={icon.url} target="_blank" rel="noopener noreferrer">
+                <Image
+                  src={icon.iconSrc}
+                  alt={`${icon.name} icon`}
+                  width={40}
+                  height={40}
+                  className="icon-circle"
+                />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>© 2024 Spencer Lepine</div>
     </footer>
   );
 }
